d3a9cec30650236caf99573327617cebfd986b69
Rob McCarthy <remccarthy@sei.cmu.edu>
2021-07-29 11:59:38 -0400

    add verbosity to preserve link printing

--- build/python/scripts/entrypoint.sh
+++ build/python/scripts/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # download the package requested by the user, parse for added urls, and then write to file 'urls'
-python -m pip download -v $PACKAGE |grep "from https" |grep "Added" | awk '{ print $4}' > urls
-
+python -m pip download -vvv beautifulsoup4 django prophet flask matplot numba numpy pandas plotly requests scikit-learn scipy seaborn statsmodels tqdm urllib3 wordcloud |grep "from https" |grep "Added" | awk '{ print $4}' > urls
